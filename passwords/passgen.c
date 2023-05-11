d060bd5f68956ffffc2d9ab7b1bd331a33d3d380
L1u2c3a4s5862 <54462476+L1u2c3a4s5862@users.noreply.github.com>
2023-05-11 15:30:42 -0400

    Add files via upload

---
+++ passwords/passgen.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <getopt.h>
+
+// Default values for password generation
+#define DEFAULT_LENGTH 12
+#define DEFAULT_COUNT 5
+#define DEFAULT_CHARS "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+{}[]|\\:;\"'<>,.?/ "
+
+// Function to generate a random password of a given length and character set
+char* generate_password(int length, const char* chars) {
+    char* password = malloc(length + 1);
+    for (int i = 0; i < length; i++) {
+        int index = rand() % strlen(chars);
+        password[i] = chars[index];
+    }
+    password[length] = '\0';
+    return password;
+}
+
+int main(int argc, char* argv[]) {
+    int length = DEFAULT_LENGTH;
+    int count = DEFAULT_COUNT;
+    const char* chars = DEFAULT_CHARS;
+    int opt;
+
+    // Command line options parsing
+    while ((opt = getopt(argc, argv, "hl:n:c:")) != -1) {
+        switch (opt) {
+            case 'h':
+                printf("Usage: %s [-h] [-l LENGTH] [-n NUMBER] [-c CHARS]\n", argv[0]);
+                printf("\nOptions:\n");
+                printf("  -h               Show this help message and exit\n");
+                printf("  -l LENGTH        Length of each password (default: \033[1;34m%d\033[1;0m)\n", DEFAULT_LENGTH);
+                printf("  -n NUMBER        Number of passwords to generate (default: \033[1;34m%d\033[1;0m)\n", DEFAULT_COUNT);
+                printf("  -c CHARS         Characters to use for password generation\n");
+                printf("                   (default: \033[1;34m\"%s\"\033[1;0m)\n", DEFAULT_CHARS);
+                return 0;
+            case 'l':
+                length = atoi(optarg);
+                break;
+            case 'n':
+                count = atoi(optarg);
+                break;
+            case 'c':
+                chars = optarg;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-h] [-l LENGTH] [-n NUMBER] [-c CHARS]\n", argv[0]);
+                return 1;
+        }
+    }
+
+    // Seed the random number generator with the current time
+    srand(time(NULL));
+
+    // Generate the passwords
+    for (int i = 0; i < count; i++) {
+        char* password = generate_password(length, chars);
+        printf("%s\n", password);
+        free(password);
+    }
+
+    return 0;
+}
